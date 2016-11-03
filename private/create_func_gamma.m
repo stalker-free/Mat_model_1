8768673e8a1da6738ce5493eaad9848e6de4fee8
stalker-free <stalker-free@users.noreply.github.com>
2016-11-03 23:33:32 +0400

    Add files via upload

--- private/create_func_gamma.m
+++ private/create_func_gamma.m
@@ -13,14 +13,14 @@ function gamma = create_func_gamma(const,da_func, x_interval)
 %
 %	See also CREATE_FUNC_DA, SYM, SOLVE, MATLABFUNCTION, class CONST.
 B = @(n, x_m)(const.b(n) .* ...
-    exp(-2.0 .* const.happa(n) .* x_m ./ ...
-    da_func(1i .* const.happa(n))));
+    exp(-2.0 .* const.happa(n) .* x_m) ./ ...
+    da_func(1i .* const.happa(n)));
 A = eye(const.N, const.N);
 
 C = @(A_matr, n, x_m)(A_matr - ...
     sum(1i .* B(n, x_m) ./ (const.happa(n) + const.happa)));
 
-m_s = max(size(x_interval));
+% m_s = max(size(x_interval));
 % gamma = zeros(const.N, m_s);
 % for j = 1:const.N
 %     for k = 1:m_s
@@ -29,10 +29,14 @@ m_s = max(size(x_interval));
 % end
 
 gamma_aux = B(1:const.N, x_interval) / C(A, 1:const.N, x_interval);
-gamma_poly = cell(1,const.N);
-for k = 1:const.N
-    gamma_poly{k} = polyfit(x_interval,gamma_aux(k,:),5);
-end
+figure
+plot(x_interval, imag(gamma_aux), '-')%, ...
+   % real(gamma_aux), imag(gamma_aux), 'ro')
+
+% gamma_poly = cell(1,const.N);
+% for k = 1:const.N
+%     gamma_poly{k} = polyfit(x_interval,gamma_aux(k,:),5);
+% end
 % for k = 1:const.N
 %     gamma_val{k} = containers.Map('KeyType', 'double', 'ValueType', 'any');
 %     for l = 1:m_s
@@ -45,9 +49,27 @@ end
 %         val(p) = gamma_val{p}(x);
 %     end
 % end
+    function val = gamma_f(g_v, x)
+        find_line_value = @(x0,x1,x2)(x1+x2*abs((x0-x1)/(x2-x1)));
+        tol = 1e-6;
+        real_s = ismembertol(real(g_v), real(x), tol);
+        imag_s = ismembertol(imag(g_v), imag(x), tol);
+        % Check if g_v already has x
+        %if(real_s(real_s ~= 0) && imag_s(imag_s ~= 0))
+        if(intersection(real_s, imag_s))
+            val = x;
+        % Generate value from line segment
+        else
+            lesser = find(g_v <= x, 1, 'last');
+            bigger = find(g_v > x);
+            val = find_line_value(real([x,lesser,bigger])) + ...
+                1i * find_line_value(imag([x,lesser,bigger]));
+        end
+    end
+
 gamma = cell(1,const.N);
 for k = 1:const.N
-    gamma{k} = @(x)polyval(gamma_poly{k},x);
+    gamma{k} = @(x)gamma_f(gamma_aux(k),x);
 end
 end
 %     function s_f = gen_s(n)
